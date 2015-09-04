79e61d5af7e8075ef44799acafa93b11c256de75
Ibrahim Eid <humazed@gmail.com>
2015-09-05 01:58:58 +0200

    change spinners in SignupActivity: by adding university spinner. and change how spinners fill with data, NOW it's fill from dataBase using helper methods in SubjectDataSource.

--- app/src/main/java/com/example/huma/al_malzma/ui/SignupActivity.java
+++ app/src/main/java/com/example/huma/al_malzma/ui/SignupActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -14,6 +16,7 @@ import android.widget.Toast;
 import com.example.huma.al_malzma.R;
 import com.example.huma.al_malzma.model.data.Faculties;
 import com.example.huma.al_malzma.parse.ParseConstants;
+import com.example.huma.al_malzma.persistence.SubjectDataSource;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
@@ -25,6 +28,8 @@ import butterknife.OnClick;
 
 
 public class SignupActivity extends AppCompatActivity {
+    public static final String TAG = SignupActivity.class.getSimpleName();
+
     @Bind(R.id.name_edit_text) EditText mNameEditText;
     @Bind(R.id.email_edit_text) EditText mEmailEditText;
     @Bind(R.id.password_edit_text) EditText mPasswordEditText;
@@ -32,7 +37,10 @@ public class SignupActivity extends AppCompatActivity {
     @Bind(R.id.progress_view) ProgressView mProgressView;
     @Bind(R.id.department_text_view) TextView mDepartmentTextView;
 
+    @Bind(R.id.faculty_text_view) TextView mFacultyTextView;
     @Bind(R.id.grade_text_view) TextView mGradeTextView;
+
+    @Bind(R.id.university_spinner) Spinner mUniversitySpinner;
     @Bind(R.id.faculty_spinner) Spinner mFacultySpinner;
     @Bind(R.id.department_spinner) Spinner mDepartmentSpinner;
     @Bind(R.id.grade_spinner) Spinner mGradeSpinner;
@@ -40,12 +48,25 @@ public class SignupActivity extends AppCompatActivity {
 
     String mFaculty, mDepartment, mGrade;
 
+    String selectedUniversity, selectedFaculty, selectedDepartment, selectedGrade;
+    String[] faculties, departments, grades;
+
+    private SubjectDataSource mSubjectDataSource;
+
+    @OnClick(R.id.test_button)
+    void test() {
+        Log.d(TAG, "test " + selectedUniversity);
+        Log.d(TAG, "test " + selectedFaculty);
+        Log.d(TAG, "test " + selectedDepartment);
+        Log.d(TAG, "test " + selectedGrade);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         ButterKnife.bind(this);
+        mSubjectDataSource = new SubjectDataSource(this);
 
         /*
         *the construct of grade which passed to parse.com is as following
@@ -55,132 +76,216 @@ public class SignupActivity extends AppCompatActivity {
         * and for the rest of departments used the first letter of them.
         */
 
+        ///////////////////////////////////////////////////////////////////////////
+        // START: Handle spinners entries and visibly.
+        ///////////////////////////////////////////////////////////////////////////
+        final String[] universities = mSubjectDataSource.getUniversities();
+        mUniversitySpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, universities));
+        mUniversitySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedUniversity = universities[position];
+                faculties = mSubjectDataSource.getFaculties(selectedUniversity);
+
+                mFacultyTextView.setVisibility(View.VISIBLE);
+                mFacultySpinner.setVisibility(View.VISIBLE);
+
+                mFacultySpinner.setAdapter(new ArrayAdapter<>(SignupActivity.this,
+                        android.R.layout.simple_spinner_dropdown_item, faculties));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         mFacultySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        mFaculty = Faculties.Engineering.FACULTY_ENGINEERING;
-                        showDepartment();
-                        break;
-                }
+                selectedFaculty = faculties[position];
+                departments = mSubjectDataSource.getDepartments(selectedUniversity, selectedFaculty);
 
+                mDepartmentTextView.setVisibility(View.VISIBLE);
+                mDepartmentSpinner.setVisibility(View.VISIBLE);
+
+                mDepartmentSpinner.setAdapter(new ArrayAdapter<>(SignupActivity.this,
+                        android.R.layout.simple_spinner_dropdown_item, departments));
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
             }
-
-            private void showDepartment() {
-                mDepartmentTextView.setVisibility(View.VISIBLE);
-                mDepartmentSpinner.setVisibility(View.VISIBLE);
-            }
         });
 
-        //Let the user choose his DepartmentTable.
         mDepartmentSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        mDepartment = Faculties.Engineering.Departments.DEPARTMENT_PREP; //prep
-                        hideGrade();
-                        break;
-                    case 1:
-                        mDepartment = Faculties.Engineering.Departments.DEPARTMENT_SYSTEMS_AND_COMPUTER_ENGINEERING;
-                        showGrade();
-                        break;
-                    case 2:
-                        mDepartment = Faculties.Engineering.Departments.DEPARTMENT_PETROL_ENGINEERING;
-                        showGrade();
-                        Toast.makeText(SignupActivity.this, R.string.coming_soon_message, Toast.LENGTH_SHORT).show();
-                        break;
-                    case 3:
-                        mDepartment = Faculties.Engineering.Departments.DEPARTMENT_ELECTRICAL_ENGINEERING;
-                        showGrade();
-                        Toast.makeText(SignupActivity.this, R.string.coming_soon_message, Toast.LENGTH_SHORT).show();
-                        break;
-                    case 4:
-                        mDepartment = Faculties.Engineering.Departments.DEPARTMENT_ARCHITECTURE_ENGINEERING;
-                        showGrade();
-                        Toast.makeText(SignupActivity.this, R.string.coming_soon_message, Toast.LENGTH_SHORT).show();
-                        break;
-                    case 5:
-                        mDepartment = Faculties.Engineering.Departments.DEPARTMENT_CIVIL_ENGINEERING;
-                        showGrade();
-                        Toast.makeText(SignupActivity.this, R.string.coming_soon_message, Toast.LENGTH_SHORT).show();
-                        break;
-                    case 6:
-                        mDepartment = Faculties.Engineering.Departments.DEPARTMENT_MECHANICAL_ENGINEERING;
-                        showGrade();
-                        Toast.makeText(SignupActivity.this, R.string.coming_soon_message, Toast.LENGTH_SHORT).show();
-                        break;
-                    case 7:
-                        mDepartment = Faculties.Engineering.Departments.DEPARTMENT_URBAN_DESIGN_ENGINEERING;
-                        showGrade();
-                        Toast.makeText(SignupActivity.this, R.string.coming_soon_message, Toast.LENGTH_SHORT).show();
-                        break;
-                    default:    //error state case.
-                        mDepartment = Faculties.ERROR;
-                        break;
+                selectedDepartment = departments[position];
+                grades = mSubjectDataSource.getGrades(selectedUniversity, selectedFaculty, selectedDepartment);
+
+                if (!grades[0].equals("0")) {
+                    mGradeTextView.setVisibility(View.VISIBLE);
+                    mGradeSpinner.setVisibility(View.VISIBLE);
+
+                    mGradeSpinner.setAdapter(new ArrayAdapter<>(SignupActivity.this,
+                            android.R.layout.simple_spinner_dropdown_item, grades));
+                } else {
+                    selectedGrade = "0";
+                    mGradeTextView.setVisibility(View.INVISIBLE);
+                    mGradeSpinner.setVisibility(View.INVISIBLE);
                 }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(SignupActivity.this);
-                builder.setTitle(getString(R.string.generic_error_title))
-                        .setMessage(R.string.department_select_message)
-                        .setPositiveButton(android.R.string.ok, null)
-                        .create().show();
-            }
 
-            private void showGrade() {
-                mGradeSpinner.setVisibility(View.VISIBLE);
-                mGradeTextView.setVisibility(View.VISIBLE);
             }
-
-            private void hideGrade() {
-                mGradeSpinner.setVisibility(View.INVISIBLE);
-                mGradeTextView.setVisibility(View.INVISIBLE);
-            }
-
         });
 
-        //let user choose the Grades.
         mGradeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        mGrade = Faculties.Engineering.Grades.GRADE_1;
-                        break;
-                    case 1:
-                        mGrade = Faculties.Engineering.Grades.GRADE_2;
-                        break;
-                    case 2:
-                        mGrade = Faculties.Engineering.Grades.GRADE_3;
-                        break;
-                    case 3:
-                        mGrade = Faculties.Engineering.Grades.GRADE_4;
-                        break;
-                    default:
-                        mGrade = Faculties.ERROR; //error.
-                        break;
-                }
+                selectedGrade = grades[position];
             }
-
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(SignupActivity.this);
-                builder.setTitle(getString(R.string.generic_error_title))
-                        .setMessage(R.string.grade_select_message)
-                        .setPositiveButton(android.R.string.ok, null)
-                        .create().show();
+
             }
         });
+
+        ///////////////////////////////////////////////////////////////////////////
+        // END.
+        ///////////////////////////////////////////////////////////////////////////
+
+//        mFacultySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                switch (position) {
+//                    case 0:
+//                        mFaculty = Faculties.Engineering.FACULTY_ENGINEERING;
+//                        showDepartment();
+//                        break;
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//
+//            private void showDepartment() {
+//                mDepartmentTextView.setVisibility(View.VISIBLE);
+//                mDepartmentSpinner.setVisibility(View.VISIBLE);
+//            }
+//        });
+//
+//        //Let the user choose his DepartmentTable.
+//        mDepartmentSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                switch (position) {
+//                    case 0:
+//                        mDepartment = Faculties.Engineering.Departments.DEPARTMENT_PREP; //prep
+//                        hideGrade();
+//                        break;
+//                    case 1:
+//                        mDepartment = Faculties.Engineering.Departments.DEPARTMENT_SYSTEMS_AND_COMPUTER_ENGINEERING;
+//                        showGrade();
+//                        break;
+//                    case 2:
+//                        mDepartment = Faculties.Engineering.Departments.DEPARTMENT_PETROL_ENGINEERING;
+//                        showGrade();
+//                        Toast.makeText(SignupActivity.this, R.string.coming_soon_message, Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case 3:
+//                        mDepartment = Faculties.Engineering.Departments.DEPARTMENT_ELECTRICAL_ENGINEERING;
+//                        showGrade();
+//                        Toast.makeText(SignupActivity.this, R.string.coming_soon_message, Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case 4:
+//                        mDepartment = Faculties.Engineering.Departments.DEPARTMENT_ARCHITECTURE_ENGINEERING;
+//                        showGrade();
+//                        Toast.makeText(SignupActivity.this, R.string.coming_soon_message, Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case 5:
+//                        mDepartment = Faculties.Engineering.Departments.DEPARTMENT_CIVIL_ENGINEERING;
+//                        showGrade();
+//                        Toast.makeText(SignupActivity.this, R.string.coming_soon_message, Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case 6:
+//                        mDepartment = Faculties.Engineering.Departments.DEPARTMENT_MECHANICAL_ENGINEERING;
+//                        showGrade();
+//                        Toast.makeText(SignupActivity.this, R.string.coming_soon_message, Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case 7:
+//                        mDepartment = Faculties.Engineering.Departments.DEPARTMENT_URBAN_DESIGN_ENGINEERING;
+//                        showGrade();
+//                        Toast.makeText(SignupActivity.this, R.string.coming_soon_message, Toast.LENGTH_SHORT).show();
+//                        break;
+//                    default:    //error state case.
+//                        mDepartment = Faculties.ERROR;
+//                        break;
+//                }
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//                AlertDialog.Builder builder = new AlertDialog.Builder(SignupActivity.this);
+//                builder.setTitle(getString(R.string.generic_error_title))
+//                        .setMessage(R.string.department_select_message)
+//                        .setPositiveButton(android.R.string.ok, null)
+//                        .create().show();
+//            }
+//
+//            private void showGrade() {
+//                mGradeSpinner.setVisibility(View.VISIBLE);
+//                mGradeTextView.setVisibility(View.VISIBLE);
+//            }
+//
+//            private void hideGrade() {
+//                mGradeSpinner.setVisibility(View.INVISIBLE);
+//                mGradeTextView.setVisibility(View.INVISIBLE);
+//            }
+//
+//        });
+//
+//        //let user choose the Grades.
+//        mGradeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                switch (position) {
+//                    case 0:
+//                        mGrade = Faculties.Engineering.Grades.GRADE_1;
+//                        break;
+//                    case 1:
+//                        mGrade = Faculties.Engineering.Grades.GRADE_2;
+//                        break;
+//                    case 2:
+//                        mGrade = Faculties.Engineering.Grades.GRADE_3;
+//                        break;
+//                    case 3:
+//                        mGrade = Faculties.Engineering.Grades.GRADE_4;
+//                        break;
+//                    default:
+//                        mGrade = Faculties.ERROR; //error.
+//                        break;
+//                }
+//            }
+//
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//                AlertDialog.Builder builder = new AlertDialog.Builder(SignupActivity.this);
+//                builder.setTitle(getString(R.string.generic_error_title))
+//                        .setMessage(R.string.grade_select_message)
+//                        .setPositiveButton(android.R.string.ok, null)
+//                        .create().show();
+//            }
+//        });
 
     }
 
