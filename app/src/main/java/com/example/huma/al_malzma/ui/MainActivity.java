fad555741d8274a76dd3dc619fc60393c7f2c5bb
Ibrahim Eid <humazed@gmail.com>
2015-09-05 14:03:43 +0200

    adapt MainActivity to use new dataBase, and git rid of huge switch case method that was doing the same work.

--- app/src/main/java/com/example/huma/al_malzma/ui/MainActivity.java
+++ app/src/main/java/com/example/huma/al_malzma/ui/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.huma.al_malzma.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -11,11 +12,11 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.huma.al_malzma.R;
-import com.example.huma.al_malzma.model.data.Faculties;
+import com.example.huma.al_malzma.model.data.JsonAttributes;
 import com.example.huma.al_malzma.parse.ParseConstants;
+import com.example.huma.al_malzma.persistence.SubjectDataSource;
 import com.github.clans.fab.FloatingActionButton;
 import com.parse.ParseUser;
 
@@ -23,17 +24,22 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     @Bind(R.id.subjects_listView) ListView mSubjectsListView;
     @Bind(R.id.empty_text_view) TextView mEmptyTextView;
     @Bind(R.id.fab) FloatingActionButton mFab;
 
 
+    private int mPreviousVisibleItem;
+
     public static ParseUser mCurrentUser;
 
-    String mFaculty, mDepartment, mGrade, mTerm;
+    SubjectDataSource mSubjectDataSource;
+
+    String mUniversity, mFaculty, mDepartment, mGrade, mTerm;
 
     String[] mSubjects;
-    private int mPreviousVisibleItem;
 
 
     @Override
@@ -41,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        mSubjectDataSource = new SubjectDataSource(this);
 
         mCurrentUser = ParseUser.getCurrentUser();
         if (mCurrentUser == null) {
@@ -52,17 +59,15 @@ public class MainActivity extends AppCompatActivity {
         // do stuff with the user
         else {
             //get the user FacultyTable, DepartmentTable and GradeTable to show him the right subjects.
+            mUniversity = mCurrentUser.getString(ParseConstants.KEY_UNIVERSITY);
             mFaculty = mCurrentUser.getString(ParseConstants.KEY_FACULTY);
             mDepartment = mCurrentUser.getString(ParseConstants.KEY_DEPARTMENT);
             mGrade = mCurrentUser.getString(ParseConstants.KEY_GRADE);
-            //2 for the second term
-            //TODO: replace it with dynamic code that know the current term using the data.
-            mTerm = Faculties.TERM_2;
+            mTerm = getCurrentTerm();
 
-            Toast.makeText(this, mGrade, Toast.LENGTH_SHORT).show();
-            Log.d("Grades: ", mGrade);
+            Log.d(TAG, "onCreate " + mUniversity + mFaculty + mDepartment + mGrade + mTerm);
 
-            mSubjects = getSubjectsArr(mFaculty, mDepartment, mGrade, mTerm);
+            mSubjects = mSubjectDataSource.getSubjects(mUniversity, mFaculty, mDepartment, mGrade, mTerm);
 
             // Set the adapter with subjects.
             mSubjectsListView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mSubjects));
@@ -78,6 +83,12 @@ public class MainActivity extends AppCompatActivity {
             });
         }
 
+    }
+
+    @NonNull
+    private String getCurrentTerm() {
+        //TODO: replace it with dynamic code that know the current term using the data.
+        return JsonAttributes.TERM_1;
     }
 
     @Override
@@ -104,82 +115,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-
-    // Get Subjects array by search in: Faculties >> Departments >> Grades >> Terms. and then get it from xml.
-    private String[] getSubjectsArr(String faculty, String department, String grade, String term) {
-        String[] subjects = new String[0];
-        //switch the user FacultyTable, DepartmentTable and GradeTable to show him the right subjects.
-        switch (faculty) {
-            case Faculties.Engineering.FACULTY_ENGINEERING:
-                switch (department) {
-                    case Faculties.Engineering.Departments.DEPARTMENT_PREP:
-                        switch (term) {
-                            case Faculties.TERM_1:
-                                break;
-                            case Faculties.TERM_2:
-                                subjects = this.getResources().getStringArray(R.array.subjects_p0_1_2);
-                                break;
-                        }
-                        break;
-                    case Faculties.Engineering.Departments.DEPARTMENT_SYSTEMS_AND_COMPUTER_ENGINEERING:
-                        switch (grade) {
-                            case Faculties.Engineering.Grades.GRADE_1:
-                                switch (term) {
-                                    case Faculties.TERM_1:
-                                        break;
-                                    case Faculties.TERM_2:
-                                        subjects = this.getResources().getStringArray(R.array.subjects_n_1_2);
-                                        break;
-                                }
-                                break;
-                            case Faculties.Engineering.Grades.GRADE_2:
-                                switch (term) {
-                                    case Faculties.TERM_1:
-                                        break;
-                                    case Faculties.TERM_2:
-                                        subjects = this.getResources().getStringArray(R.array.subjects_n_2_2);
-                                        break;
-                                }
-                                break;
-                            case Faculties.Engineering.Grades.GRADE_3:
-                                switch (term) {
-                                    case Faculties.TERM_1:
-                                        break;
-                                    case Faculties.TERM_2:
-                                        subjects = this.getResources().getStringArray(R.array.subjects_n_3_2);
-                                        break;
-                                }
-                                break;
-                            case Faculties.Engineering.Grades.GRADE_4:
-                                switch (term) {
-                                    case Faculties.TERM_1:
-                                        break;
-                                    case Faculties.TERM_2:
-                                        subjects = this.getResources().getStringArray(R.array.subjects_n_4_2);
-                                        break;
-                                }
-                                break;
-                        }
-                        break;
-                    case Faculties.Engineering.Departments.DEPARTMENT_PETROL_ENGINEERING:
-                        break;
-                    case Faculties.Engineering.Departments.DEPARTMENT_ELECTRICAL_ENGINEERING:
-                        break;
-                    case Faculties.Engineering.Departments.DEPARTMENT_ARCHITECTURE_ENGINEERING:
-                        break;
-                    case Faculties.Engineering.Departments.DEPARTMENT_CIVIL_ENGINEERING:
-                        break;
-                    case Faculties.Engineering.Departments.DEPARTMENT_MECHANICAL_ENGINEERING:
-                        break;
-                    case Faculties.Engineering.Departments.DEPARTMENT_URBAN_DESIGN_ENGINEERING:
-                        break;
-                }
-                break;
-        }
-        return subjects;
-    }
-
 
 }
 
